51649252874c4824f6db02721b9d94e6b7a3d34c
Robin Ondroušek <robin.ondrousek@gmail.com>
2026-02-17 17:07:51 +0100

    render

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
+const distPath = path.join(__dirname, 'dist');
+
 // Tímto řekneš serveru, aby poslal soubory hry (ze složky dist) prohlížeči
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(distPath));
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -27,6 +30,17 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('Hráč odpojen');
     });
+});
+
+// Fallback pro SPA - pokud se nenajde soubor (např. při refresh), vrátí index.html
+// Toto také vyřeší chybu "Cannot GET /", pokud existuje index.html
+app.get('*', (req, res) => {
+    const indexPath = path.join(distPath, 'index.html');
+    if (fs.existsSync(indexPath)) {
+        res.sendFile(indexPath);
+    } else {
+        res.status(404).send('CHYBA: Soubor index.html nebyl nalezen ve složce dist. Spusťte "npm run build".');
+    }
 });
 
 const PORT = process.env.PORT || 3000;
